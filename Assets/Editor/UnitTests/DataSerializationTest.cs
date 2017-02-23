63c140a24bd3d7b4a705aafa0eb401f869b5a6c1
Simon Boigelot <sboigelot@gmail.com>
2017-02-23 23:37:49 +0100

    Some tests

--- Assets/Editor/UnitTests/DataSerializationTest.cs
+++ Assets/Editor/UnitTests/DataSerializationTest.cs
@@ -1,100 +1,100 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using Assets.Scripts.Models;
 using Assets.Scripts.Serialization;
 using NUnit.Framework;
-using UnityEditor.VersionControl;
 
-public class DataSerializationTest
+namespace Assets.Editor.UnitTests
 {
-    [Test]
-    public void SerializeCardTest()
+    public class DataSerializationTest
     {
-        var data = new Card
+        [Test]
+        public void SerializeCardTest()
         {
-            Name = "TestCard",
-            DescriptionTextLocalCode = "description"
-        };
-
-        MemoryStream ms = new MemoryStream();
-        DataSerializer.Instance.Serialize(ms, data);
-        ms.Flush();
-
-        ms.Position = 0;
-        var sr = new StreamReader(ms);
-        var xml = sr.ReadToEnd();
-        Console.WriteLine(xml);
-
-        ms.Position = 0;
-        var data2 = DataSerializer.Instance.DeSerialize<Card>(ms);
-
-        Assert.AreEqual(data.Name, data2.Name);
-        Assert.AreEqual(data2.DescriptionTextLocalCode, "description");
-    }
-
-    [Test]
-    public void SerializeCardList()
-    {
-        List<Card> data = new List<Card>
-        {
-            new Card { Name = "Test1" },
-            new Card { Name = "Test2" },
-            new Card { Name = "Test3" }
-        };
-
-        MemoryStream ms = new MemoryStream();
-        DataSerializer.Instance.Serialize(ms, data);
-        ms.Flush();
-        
-        ms.Position = 0;
-        var sr = new StreamReader(ms);
-        var xml = sr.ReadToEnd();
-        Console.WriteLine(xml);
-
-        ms.Position = 0;
-        var data2 = DataSerializer.Instance.DeSerialize<List<Card>>(ms);
-
-        Assert.AreEqual(data.Count, data2.Count);
-        Assert.AreEqual(data[0].Name, data2[0].Name);
-    }
-
-    [Test]
-    public void SerializeDeckTest()
-    {
-        var data = new Deck()
-        {
-            Name = "TestDeck",
-            Cards = new List<Card>
+            var data = new Card
             {
-                new Card { Name = "Test1" },
-                new Card { Name = "Test2" },
-                new Card { Name = "Test3" }
-            }
-        };
+                Name = "TestCard",
+                DescriptionTextLocalCode = "description"
+            };
 
-        MemoryStream ms = new MemoryStream();
-        DataSerializer.Instance.Serialize(ms, data);
-        ms.Flush();
+            var ms = new MemoryStream();
+            DataSerializer.Instance.Serialize(ms, data);
+            ms.Flush();
 
-        ms.Position = 0;
-        var sr = new StreamReader(ms);
-        var xml = sr.ReadToEnd();
-        Console.WriteLine(xml);
+            ms.Position = 0;
+            var sr = new StreamReader(ms);
+            string xml = sr.ReadToEnd();
+            Console.WriteLine(xml);
 
-        ms.Position = 0;
-        var data2 = DataSerializer.Instance.DeSerialize<Deck>(ms);
+            ms.Position = 0;
+            var data2 = DataSerializer.Instance.DeSerialize<Card>(ms);
 
-        Assert.AreEqual(data.Name, data2.Name);
-        Assert.AreEqual(data.Cards[0].Name, data2.Cards[0].Name);
-    }
+            Assert.AreEqual(data.Name, data2.Name);
+            Assert.AreEqual(data2.DescriptionTextLocalCode, "description");
+        }
 
-    [Test]
-    public void DerializeDeckTest()
-    {
-        string xml =
-        @"<?xml version='1.0' encoding='Windows-1252'?>
+        [Test]
+        public void SerializeCardList()
+        {
+            var data = new List<Card>
+            {
+                new Card {Name = "Test1"},
+                new Card {Name = "Test2"},
+                new Card {Name = "Test3"}
+            };
+
+            var ms = new MemoryStream();
+            DataSerializer.Instance.Serialize(ms, data);
+            ms.Flush();
+
+            ms.Position = 0;
+            var sr = new StreamReader(ms);
+            string xml = sr.ReadToEnd();
+            Console.WriteLine(xml);
+
+            ms.Position = 0;
+            var data2 = DataSerializer.Instance.DeSerialize<List<Card>>(ms);
+
+            Assert.AreEqual(data.Count, data2.Count);
+            Assert.AreEqual(data[0].Name, data2[0].Name);
+        }
+
+        [Test]
+        public void SerializeDeckTest()
+        {
+            var data = new Deck
+            {
+                Name = "TestDeck",
+                Cards = new List<Card>
+                {
+                    new Card {Name = "Test1"},
+                    new Card {Name = "Test2"},
+                    new Card {Name = "Test3"}
+                }
+            };
+
+            var ms = new MemoryStream();
+            DataSerializer.Instance.Serialize(ms, data);
+            ms.Flush();
+
+            ms.Position = 0;
+            var sr = new StreamReader(ms);
+            string xml = sr.ReadToEnd();
+            Console.WriteLine(xml);
+
+            ms.Position = 0;
+            var data2 = DataSerializer.Instance.DeSerialize<Deck>(ms);
+
+            Assert.AreEqual(data.Name, data2.Name);
+            Assert.AreEqual(data.Cards[0].Name, data2.Cards[0].Name);
+        }
+
+        [Test]
+        public void DerializeDeckTest()
+        {
+            string xml =
+                @"<?xml version='1.0' encoding='Windows-1252'?>
         <Deck Name='Default'>
             <Card Name='Test Card 1' SpriteName='' DescriptionTextLocalCode='' LeftOptionTextLocalCode='' RightOptionTextLocalCode=''>
                 <LeftEffect TurnDelay='0' HasDialog='true' DialogSpriteName='' DialogTextLocalCode=''
@@ -114,24 +114,25 @@ public class DataSerializationTest
             </Card>
         </Deck>".Replace("'", "\"");
 
-        using (Stream sr = GenerateStreamFromString(xml))
-        {
-            var data2 = DataSerializer.Instance.DeSerialize<Deck>(sr);
+            using (var sr = GenerateStreamFromString(xml))
+            {
+                var data2 = DataSerializer.Instance.DeSerialize<Deck>(sr);
 
-            Assert.AreEqual("Default", data2.Name);
-            Assert.AreEqual(2, data2.Cards.Count);
-            Assert.AreEqual(2, data2.Cards[0].LeftEffects.Count);
-            Assert.AreEqual(1, data2.Cards[0].RightEffects.Count);
+                Assert.AreEqual("Default", data2.Name);
+                Assert.AreEqual(2, data2.Cards.Count);
+                Assert.AreEqual(2, data2.Cards[0].LeftEffects.Count);
+                Assert.AreEqual(1, data2.Cards[0].RightEffects.Count);
+            }
         }
-    }
 
-    public Stream GenerateStreamFromString(string s)
-    {
-        MemoryStream stream = new MemoryStream();
-        StreamWriter writer = new StreamWriter(stream);
-        writer.Write(s);
-        writer.Flush();
-        stream.Position = 0;
-        return stream;
+        public Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
     }
 }
